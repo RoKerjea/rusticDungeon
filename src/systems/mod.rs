e639a7d39c74904e361a17e54240ad753d240347
RoKerjea <kerjeanr@gmail.com>
2023-12-01 17:41:15 +0100

    feat : added combat system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,10 +2,10 @@ mod map_render;
 mod player_input;
 mod random_move;
 mod entity_render;
-mod collisions;
 mod end_turn;
 mod hud;
 mod tooltips;
+mod combat;
 mod movements;
 
 use crate::prelude::*;
@@ -22,9 +22,9 @@ pub fn build_input_scheduler() -> Schedule {
 }
 pub fn build_player_scheduler() -> Schedule {
 	Schedule::builder()
-		.add_system(movements::movement_system())
+		.add_system(combat::combat_system())
 		.flush()
-		.add_system(collisions::collisions_system())
+		.add_system(movements::movement_system())
 		.flush()
 		.add_system(map_render::map_render_system())
 		.add_system(entity_render::entity_render_system())
@@ -36,6 +36,8 @@ pub fn build_player_scheduler() -> Schedule {
 pub fn build_monster_scheduler() -> Schedule {
 	Schedule::builder()
 		.add_system(random_move::random_move_system())
+		.flush()
+		.add_system(combat::combat_system())
 		.flush()
 		.add_system(movements::movement_system())
 		.flush()
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -12,6 +12,8 @@ pub fn player_input(
 {
 	let mut players = <(Entity, &Point)>::query()
 		.filter(component::<Player>());
+
+	
 	if let Some(key) = key {
 		let delta = match key {
 			VirtualKeyCode::Left => Point::new(-1, 0),
@@ -24,6 +26,34 @@ pub fn player_input(
 			let destination = *pos + delta;
 			commands.push(((), WantsToMove{entity: *entity, destination}));
 		});
+		let (player_entity, destination) = players
+			.iter(ecs)
+			.find_map(|(entity, pos) | Some((*entity, *pos + delta)) )
+			.unwrap();
+		let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+		if delta.x != 0 || delta.y != 0 {//probably could be refactored for better lisibility
+			let mut hit_something = false;
+			enemies
+				.iter(ecs)
+				.filter(|(_, pos)| {
+					**pos == destination
+				})
+				.for_each(|(entity, _) | {
+					hit_something = true;
+					commands
+						.push(((), WantsToAttack{
+							attacker: player_entity,
+							victim: *entity,
+						}));
+				});
+			if !hit_something {
+				commands
+					.push(((), WantsToMove {
+						entity: player_entity,
+						destination
+					}));
+			}
+		}
 		*turn_state = TurnState::PlayerTurn;
 	}
 }
